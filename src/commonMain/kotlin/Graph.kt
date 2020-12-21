c82c26b9a155c49e849ed01346b009b6974746bc
Garrett van Wageningen <ghvanw283@gmail.com>
2020-12-20 20:38:36 -0600

    wip

--- src/commonMain/kotlin/Graph.kt
+++ src/commonMain/kotlin/Graph.kt
@@ -140,4 +140,6 @@ fun <A> shortestPath(paths: Map<A, A>, from: A): Sequence<A> =
         }
     }
 
-fun <A> AdjacencyMap<A>.dijkstraShortestPathsTo(vertex: A): Pair<Map<A, A>, Map<A, Int>> =
+//fun <A> relax(edge: WeightedEdge<A>, vertex: A)
+
+//fun <A> AdjacencyMap<A>.dijkstraShortestPathsTo(vertex: A): Pair<Map<A, A>, Map<A, Int>> =
